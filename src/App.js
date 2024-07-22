05b65b93f64c5340e402bd39699774faf6d0a241
cheeeeze <nyj9349@hanmail.net>
2024-07-22 20:58:59 +0900

    modify css

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <span>Hello World!</span>
+          <button>button</button>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/*  <p>*/}
+      {/*    Edit <code>src/App.js</code> and save to reload.*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
       </header>
     </div>
   );
