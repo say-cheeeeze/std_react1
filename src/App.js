62f0d0d5ebe28547e647fc5fa20b52d50856eb78
cheeeeze <nyj9349@hanmail.net>
2024-07-22 21:48:21 +0900

    add props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <span>Hello World!</span>
-          <button>button</button>
-        <img src={logo} className="App-logo" alt="logo" />
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.js</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      </header>
-    </div>
-  );
+	const topicList = [
+		{ id : 1, title : 'html', body : 'html is...'},
+		{ id : 2, title : 'css', body : 'css is...'},
+		{ id : 3, title : 'js', body : 'javascript is...'},
+	]
+	return (
+		<div className="rootDiv">
+			<Header title="WEB"/>
+			<Nav list={topicList}></Nav>
+			<Article title="Welcome" body="Hello, WEB" />
+		</div>
+	);
+}
+
+function Header( props ) {
+	return (
+		<header>
+			<h1>
+				<a href="/">{ props.title }</a>
+			</h1>
+		</header>
+	)
+	
+}
+
+function Nav( props ) {
+	const list = [];
+	for( let item of props.list ) {
+		list.push( <li key={item.id} id={item.id}><a href={'/read/' + item.id}>{ item.title }</a></li> );
+	}
+	
+	return <nav>
+		<ol>
+			{ list }
+		</ol>
+	</nav>;
+}
+
+function Article( prop ) {
+	return (
+		<article>
+			<h2>{ prop.title }</h2>
+			{ prop.body }
+		</article>
+	)
 }
 
 export default App;
