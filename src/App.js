8ee40e8d880c499c296ce58a9047bfb5d0b4d70c
cheeeeze <nyj9349@hanmail.net>
2024-07-23 22:02:29 +0900

    원본데이터는 유지하고 다른 것을 할당하여 state 를 감지하도록 한다.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function App() {
 	
-	const [mode, setModeFunc] = useState( 'WELCOME' );
+	const [ mode, setModeFunc ] = useState( 'WELCOME' );
+	const [ id, setId ] = useState( null );
+	const [ nextId, setNextId ] = useState(4);
+	const [ topicList, setTopicList ] = useState( [
+		{ id : 1, title : 'HTML', body : 'html is...' },
+		{ id : 2, title : 'CSS', body : 'css is...' },
+		{ id : 3, title : 'javascript', body : 'javascript is...' },
+	] );
 	
-	const topicList = [
-		{ id : 1, title : 'html', body : 'html is...'},
-		{ id : 2, title : 'css', body : 'css is...'},
-		{ id : 3, title : 'js', body : 'javascript is...'},
-	]
+	let title = 'WELCOME';
+	let body = 'Hello, WEB';
+	let content = <Article title={title} body={body}/>;
 	
-	let content = null;
-	if ( mode === 'WELCOME' ) {
-		content = <Article title="Welcome" body="Hello, WEB" />;
+	if ( mode === 'READ' ) {
+		for( let i = 0; i < topicList.length; i++ ) {
+			if ( topicList[i].id === id ) {
+				title = topicList[i].title;
+				body = topicList[i].body;
+			}
+		}
+		content = <Article title={title} body={body}/>;
 	}
-	else if ( mode === 'READ') {
-		content = <Article title="Read" body="Hello, READ" />;
+	else if ( mode === 'CREATE' ) {
+		content = <Create onCreate={ (title,body) => {
+			let _topicList = [ ...topicList ];
+			_topicList.push( {
+				id : nextId,
+				title : title,
+				body : body,
+			});
+			setNextId( nextId + 1 );
+			setTopicList( _topicList );
+		}}></Create>;
 	}
 	
 	return (
 		<div className="rootDiv">
-			<Header title="WEB" onCustomEvent={() => {
-				setModeFunc('WELCOME');
-			}}/>
-			<Nav list={topicList} onCustomEvent={( id ) => {
-				console.log( id );
-				setModeFunc('READ');
-			}}></Nav>
-			{content}
+			<Header title="WEB" onChangeMode={ () => {
+				setModeFunc( 'WELCOME' );
+			} }/>
+			<Nav list={ topicList } onChangeMode={ ( _id ) => {
+				setModeFunc( 'READ' );
+				setId( _id );
+			} }></Nav>
+			{ content }
+			
+			<div className="createArea" style={{marginTop:'10px'}}>
+				<a href="/create" onClick={ event => {
+					event.preventDefault();
+					setModeFunc( 'CREATE' );
+				} }>추가(Create)</a>
+			</div>
+			
 		</div>
 	);
 }
@@ -37,7 +64,7 @@ function Header( props ) {
 	return (
 		<header>
 			<h1>
-				<a href="/" onClick={props.onCustomEvent}>{ props.title }</a>
+				<a href="/" onClick={ props.onChangeMode }>{ props.title }</a>
 			</h1>
 		</header>
 	)
@@ -45,13 +72,13 @@ function Header( props ) {
 
 function Nav( props ) {
 	const list = [];
-	for( let item of props.list ) {
+	for ( let item of props.list ) {
 		list.push(
-			<li key={item.id} id={item.id} onClick={ e => {
+			<li key={ item.id } id={ item.id } onClick={ e => {
 				e.preventDefault();
-				props.onCustomEvent( item.id );
+				props.onChangeMode( item.id );
 			} }>
-				<a href={'/read/' + item.id}>{ item.title }</a>
+				<a href={ '/read/' + item.id }>{ item.title }</a>
 			</li>
 		);
 	}
@@ -68,6 +95,31 @@ function Article( prop ) {
 			{ prop.body }
 		</article>
 	)
+}
+
+function Create( props ) {
+	return (
+		<article>
+			<h2>Create(추가)</h2>
+			<form onSubmit={ e => {
+				e.preventDefault();
+				let title = document.getElementById( 'formTitle' ).value;
+				let body = document.getElementById( 'formBody' ).value;
+				let value = document.getElementById( 'formValue' ).value;
+				props.onCreate( title, body );
+			}}>
+				<div>
+					<input type="text" id="formTitle" name="title" placeholder="title"/>
+				</div>
+				<div>
+					<textarea name="body" id="formBody" placeholder="body"></textarea>
+				</div>
+				<div>
+					<button type="submit" id="formValue" value="Create">create</button>
+				</div>
+			</form>
+		</article>
+	);
 }
 
 export default App;
